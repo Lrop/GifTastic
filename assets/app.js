c718dcc495c6f87b211a890d86b4042810c4fe00
Lrop <Lagropner04@gmail.com>
2019-06-17 15:17:42 -0600

    saving progress and minor fixes

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,42 @@
+var topics = ["Boats", "Planes", "Trains"];
+var numberOfGIFs = 10;
+var cutOffRating = "R";
+
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("cartoon-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	//$(".cartoon-button").unbind("click");
+
+	$(".cartoon-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
 
 function getGif (){
 
-var input = $('#search-text').val();
-
-//var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=20");
+var input = $('#cartoon-show').val();
 
 var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=20");
 
 queryURL.done(function(response) {
+
 
   console.log("success got data". response);
 
@@ -22,3 +51,22 @@ queryURL.done(function(response) {
 
 console.log(response);
 }
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#cartoon-show").val().trim());
+    $("#cartoon-show").val("");
+    
+	});
+});
