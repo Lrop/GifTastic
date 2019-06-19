981aac9af26f3ba8ddd679d6b7b9365f3a7f3618
Lrop <Lagropner04@gmail.com>
2019-06-19 13:09:37 -0600

    saving progress

--- assets/app.js
+++ assets/app.js
@@ -1,39 +1,44 @@
-var buttons = ["Boats", "Planes", "Trains"];
-var gifRating = 'R';
+var buttons = ["Planes", "Trains", "Automobiles", "Charpings", "Cacti", "PabstBlueRibbon"];
 
+
+function getGif(){
+
+  var input = $('#gif-show').val();
+
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=12";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  .then(function(response){
+
+    
+    var results = response.rating;
+    console.log(results);
+    var giphy = response.data;
+    console.log(response.data);
+    for ( i in giphy)
+   {
+   // var p = $("<p>").text("Rating: " + results[i].rating);
+
+    $('.inner').append("<img src='"+giphy[i].images.original.url+"'/>")
+  }
+
+  });
+
+}
 function renderButtons(){
 	for(var i = 0; i < buttons.length; i++) {
 		var newButton = $("<button>");
 		newButton.addClass("btn");
-		newButton.addClass("cartoon-button");
+		newButton.addClass("gif-button");
 		newButton.text(buttons[i]);
 		$("#button-container").append(newButton);
 	}
 };
- 
-function getGif (){
-
-var input = $('#gif-show').val();
-
-var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&rating=" + gifRating + "&limit=10");
-
-queryURL.done(function(response) {
-
-  
-
-  console.log("success got data". response);
-
-  var giphy = response.data;
-
-  for ( i in giphy)
-   {
-
-    $('.inner').append("<img src='"+giphy[i].images.original.url+"'/>")
-  }
-});
-
-console.log(response);
-}
 
 function addButton(show){
 	if(buttons.indexOf(show) === -1) {
@@ -53,5 +58,28 @@ $(document).ready(function(){
     
 	});
 });
+
+
+//function getGif (){
+
+//var input = $('#gif-show').val();
+
+//var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&rating=&limit=10");
+
+//queryURL.done(function(response) {
+
+  
+  //console.log("success got data". response);
+
+  //var giphy = response.data;
+
+  //for ( i in giphy)
+   //{
+
+   // $('.inner').append("<img src='"+giphy[i].images.original.url+"'/>")
+ // }
+//});
+
+//}
 
 
