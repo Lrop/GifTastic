d3c3820e8b5a2a8ac0f521a0a146577f5c1d198b
Lrop <Lagropner04@gmail.com>
2019-06-20 14:50:15 -0600

    button issue

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,44 @@
 var buttons = ["Planes", "Trains", "Automobiles", "Charpings", "Cacti", "PabstBlueRibbon", "Beer", "RollerCoaster"];
 
+//<-------------------------------------------------------------->
+function renderButtons(){
+	for(var i = 0; i < buttons.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("gif-button");
+		newButton.text(buttons[i]);
+		$("#button-container").append(newButton);
+	}
+};
+//<-------------------------------------------------------------->
+function addButton(show){
+	if(buttons.indexOf(show) === -1) {
+		buttons.push(show);
+		$("#button-container").empty();
+    renderButtons();
+    console.log(buttons);
+	}
+}
+
+//<-------------------------------------------------------------->
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#gif-show").val().trim());
+    $("#gif-show").val("");
+    
+	});
+});
+
 function getGif(){
+  
+  //var input = $('#gif-show')
+  let input = $(this).attr('btn-name');
+  //var input = $('#gif-show').val();
 
-  var input = $('#gif-show').val();
-  //let input = $(this).attr('btn-name');
 
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=12";
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=8";
 
   $.ajax({
     url: queryURL,
@@ -27,49 +60,18 @@ function getGif(){
     let pImg = $('<img>');
     pImg.attr("src", result[i].images.original.url);
     gifDiv.append(p);
-   // gifDiv.append(pImg);
-
-
-    //$(".inner").preprend(gifDiv);
-    //$('.rating').append(p);
+    
+  
     $('#gif-section').append("<img src='"+result[i].images.original.url+"'/>")
     $("#gif-section").append(gifDiv);
-
+   
   }
-
+  
   });
 
 }
 
 
-//<-------------------------------------------------------------->
-function renderButtons(){
-	for(var i = 0; i < buttons.length; i++) {
-		var newButton = $("<button>");
-		newButton.addClass("btn");
-		newButton.addClass("gif-button");
-		newButton.text(buttons[i]);
-		$("#button-container").append(newButton);
-	}
-};
-//<-------------------------------------------------------------->
-function addButton(show){
-	if(buttons.indexOf(show) === -1) {
-		buttons.push(show);
-		$("#button-container").empty();
-		renderButtons();
-	}
-}
 
-//<-------------------------------------------------------------->
-$(document).ready(function(){
-	renderButtons();
-	$("#submit").on("click", function(){
-		event.preventDefault();
-		addButton($("#gif-show").val().trim());
-    $("#gif-show").val("");
-    
-	});
-});
 
 
