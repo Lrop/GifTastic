e421f1613bcee6eb8c98750c2450298a27760dfc
Lrop <Lagropner04@gmail.com>
2019-06-20 11:26:06 -0600

    savng progess

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,9 @@ var buttons = ["Planes", "Trains", "Automobiles", "Charpings", "Cacti", "PabstBl
 function getGif(){
 
   var input = $('#gif-show').val();
+  //let input = $(this).attr('btn-name');
 
   var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=w1S2aIWlkRlLJu0hsXW7RjISiJS93vYU&limit=12";
-
 
   $.ajax({
     url: queryURL,
@@ -14,13 +14,26 @@ function getGif(){
 
   .then(function(response){
 
-    var giphy = response.data;
+    console.log(queryURL);
+    var result = response.data;
     
-    for ( i in giphy)
-   {
     
+   // for ( i in giphy)
+   for ( let i = 0; i < result.length; i++) {
+    
+    let gifDiv = $('<div>');
+    let p = $('<p>').text("Rating: " + result[i].rating);
+    console.log(p);
+    let pImg = $('<img>');
+    pImg.attr("src", result[i].images.original.url);
+    gifDiv.append(p);
+   // gifDiv.append(pImg);
 
-    $('.inner').append("<img src='"+giphy[i].images.original.url+"'/>")
+
+    //$(".inner").preprend(gifDiv);
+    //$('.rating').append(p);
+    $('#gif-section').append("<img src='"+result[i].images.original.url+"'/>")
+    $("#gif-section").append(gifDiv);
 
   }
 
@@ -29,7 +42,7 @@ function getGif(){
 }
 
 
-//<---------RENDERING BUTTONS-------------------------------------------->
+//<-------------------------------------------------------------->
 function renderButtons(){
 	for(var i = 0; i < buttons.length; i++) {
 		var newButton = $("<button>");
@@ -39,7 +52,7 @@ function renderButtons(){
 		$("#button-container").append(newButton);
 	}
 };
-//<----------------------------------------------------->
+//<-------------------------------------------------------------->
 function addButton(show){
 	if(buttons.indexOf(show) === -1) {
 		buttons.push(show);
@@ -48,11 +61,11 @@ function addButton(show){
 	}
 }
 
-//<----------------------------------------------------->
+//<-------------------------------------------------------------->
 $(document).ready(function(){
 	renderButtons();
 	$("#submit").on("click", function(){
-    event.preventDefault();
+		event.preventDefault();
 		addButton($("#gif-show").val().trim());
     $("#gif-show").val("");
     
